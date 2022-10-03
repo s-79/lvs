ae02cb7c7fa0d57066f2e1562d30045f0392b0ed
s-79 <sebastien.trouve@mailo.com>
2022-10-03 11:10:28 +0200

    Change table & scroll

---
+++ js/albums.js
@@ -0,0 +1,371 @@
+const albums =
+[
+    {
+        "album": "jdtpt",
+        "num": "01",
+        "titre": "Intro",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2001.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "02",
+        "titre": "Jamais debout toujours par terre",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2002%20-%20Jamais%20debout%20toujours%20par%20terre.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "03",
+        "titre": "La nuit avec vous",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2003%20-%20La%20nuit%20avec%20vous.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "04",
+        "titre": "Mon dieu, mon dieu",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2004%20-%20Mon%20dieu,%20mon%20dieu.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "05",
+        "titre": "Pierre Bunk",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2005%20-%20Pierre%20Bunk.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "06",
+        "titre": "Déstinée",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2006%20-%20Destinee.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "07",
+        "titre": "Petite chouille",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2007%20-%20Petite%20chouille.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "08",
+        "titre": "L'amnésique",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2008%20-%20L%20amnesique.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "09",
+        "titre": "À vous tous #1",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2009%20-%20A%20vous%20tous%201.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "10",
+        "titre": "À vous tous #2",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2010%20-%20A%20vous%20tous%202.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "11",
+        "titre": "L'assistant",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2011%20-%20L%20assistant.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "12",
+        "titre": "C'est mon choix",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2012%20-%20C%20est%20mon%20choix.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "13",
+        "titre": "Le sceptre",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2013%20-%20Le%20sceptre.ogg"
+    },
+    {
+        "album": "jdtpt",
+        "num": "14",
+        "titre": "Paris pas cher",
+        "url":"Jamais%20debout%20toujours%20par%20terre/Les%20Vieilles%20Salopes%20-%2014%20-%20Paris%20pas%20cher.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "01",
+        "titre": "Intro",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2001%20-%20..ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "02",
+        "titre": "Drame urbain",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2002%20-%20Drame%20urbain.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "03",
+        "titre": "La grosse tâche",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2003%20-%20La%20grosse%20tache.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "04",
+        "titre": "La minute",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2004%20-%20La%20minute.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "05",
+        "titre": "A qu'c'est bon !",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2005%20-%20A%20qucest%20bon.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "06",
+        "titre": "Je m'suis fait une crête",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2006%20-%20Jme%20suis%20fait%20une%20crete.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "07",
+        "titre": "Punk",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2007%20-%20Punk.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "08",
+        "titre": "Envoie sa soeur",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2008%20-%20Envoie%20sa%20soeur.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "09",
+        "titre": "Manifeste",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2009%20-%20Manifeste.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "10",
+        "titre": "Juste je me permets",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2010%20-%20Juste%20me%20permets.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "11",
+        "titre": "Je ne voulais pas",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2011%20-%20Je%20ne%20voulais%20pas.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "12",
+        "titre": "Marie",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2012%20-%20Marie.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "13",
+        "titre": "Psychiatrie",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2013%20-%20Psychiatrie.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "14",
+        "titre": "Mort à la guerre",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2014%20-%20Mort%20a%20la%20guerre.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "15",
+        "titre": "Justice 2V",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2015%20-%20Justice%202V.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "16",
+        "titre": "Contre culture",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2016%20-%20Contre%20culture.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "17",
+        "titre": "La veuve",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2017%20-%20La%20veuve.ogg"
+    },
+    {
+        "album": "amtamv",
+        "num": "18",
+        "titre": "Roger",
+        "url":"Aime-moi%20tendre%20Aime-moi%20vrai/Les%20Vieilles%20Salopes%20-%2018%20-%20Roger.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "01",
+        "titre": "Victory (The Toy Dolls)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2001.%20Victory%20(The%20Toy%20Dolls)%20061026.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "02",
+        "titre": "The Guns of Brixton (The Clash)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2002.%20The%20Guns%20of%20Brixton%20(The%20Clash)%20061026.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "03",
+        "titre": "No way (The Adolescents)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2003.%20No%20way%20(The%20Adolescents)%20061026.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "04",
+        "titre": "Rock n'roll (Gary Glitter)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2004.%20Rock%20n%20roll%20(Gary%20Glitter)%20060722.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "05",
+        "titre": "All shook up (Elvis Presley)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2005.%20All%20shook%20up%20(Elvis%20Presley)%20%20050611.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "06",
+        "titre": "Blister in the Sun (Violent Femmes)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2006.%20Blister%20in%20the%20Sun%20(Violent%20Femmes)%20050611.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "07",
+        "titre": "Where’s Captain Kirk (Spizzenergi)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2007.%20Where%E2%80%99s%20Captain%20Kirk%20(Spizzenergi)%20050611.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "08",
+        "titre": "Chinese Takaway (The Adicts)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2008.%20Chinese%20Takaway%20(The%20Adicts)%20050611.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "09",
+        "titre": "Louie Louie (Richard Berry)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2009.%20Louie%20Louie%20(Richard%20Berry)%20040925.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "10",
+        "titre": "Zu Atrapatu Arte (Kortatu)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2010.%20Zu%20Atrapatu%20Arte%20(Kortatu)%20040613.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "11",
+        "titre": "Numbers (The Adicts)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2011.%20Numbers%20(The%20Adicts)%20040509.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "12",
+        "titre": "Lapin Billy (Ludwig von 88)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2012.%20Lapin%20Billy%20(Ludwig%20von%2088)%202003.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "13",
+        "titre": "Un punk en hiver (Les Wampas)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2013.%20Comme%20un%20punk%20en%20hiver%20(Les%20Wampas)%20030913.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "14",
+        "titre": "Police Story (Anti-Flag)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2014.%20Police%20Story%20(Anti-Flag)%20020420.ogg"
+    },
+    {
+        "album": "reprises_live",
+        "num": "15",
+        "titre": "Submission (Sex pistols)",
+        "url":"Live/Les%20Vieilles%20Salopes%20-%2015.%20Submission%20(Sex%20pistols)%202001.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "01",
+        "titre": "Tsunami (Les Vieilles Salopes)",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2001.%20Tsunami%20061026.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "02",
+        "titre": "Couronne (Les Vieilles Salopes)",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2002.%20Couronne%20061024.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "03",
+        "titre": "Déstinée (Les Vieilles Salopes)",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2003.%20D%C3%A9stin%C3%A9e%202002.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "04",
+        "titre": "Numbers (The Adicts)",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2004.%20Numbers%20(The%20Adicts).mp3"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "05",
+        "titre": "Where’s Captain Kirk (Spizzenergi)",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2005.%20Where%20s%20Captain%20Kirk%20(Spizzenergi).mp3"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "06",
+        "titre": "La grosse tâche → Larsen is asylum",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2006.%20La%20grosse%20t%C3%A2che%20-%20Compil%20Larsen%20is%20asylum.mp3"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "07",
+        "titre": "L'assistant → Kill your elite",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2007.%20L%20assistant%20-%20Compil%20Kill%20your%20elite.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "08",
+        "titre": "Punk → Tout va très bien en France",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2008.%20Punk%20-%20Compil%20Tout%20va%20tr%C3%A8s%20bien%20en%20France.mp3"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "09",
+        "titre": "Jamais debout... → Bar à toutous",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2009.%20Jamais%20debout%20toujours%20par%20terre%20-%20Compil%20Bar%20%C3%A0%20toutous.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "10",
+        "titre": "Elle me saoule → Démo 2002",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2010.%20Elle%20me%20saoule%20-%20D%C3%A9mo%202002.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "11",
+        "titre": "VS Collection → Démo 2002",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2011.%20VS%20Collection%20-%20D%C3%A9mo%202002.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "12",
+        "titre": "Elle a couché → Démo 2002",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2012.%20Elle%20a%20couch%C3%A9%20-%20D%C3%A9mo%202002.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "13",
+        "titre": "Bienvenue → Démo 2001",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2013.%20Bienvenue%20-%20D%C3%A9mo%202001.ogg"
+    },
+    {
+        "album": "inedits_demos",
+        "num": "14",
+        "titre": "Douce France → Démo 2001",
+        "url":"Reprises%20Compils%20Demos/Les%20Vieilles%20Salopes%20-%2014.%20Douce%20France%20-%20D%C3%A9mo%202001.ogg"
+    }
+]
+
+export default albums;
